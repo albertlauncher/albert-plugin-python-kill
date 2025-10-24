63bcd3dccb8744e60cb1d9a55ac0b75dac6cea3d
Manuel Schneider <manuelschneid3r@gmail.com>
2025-10-24 16:35:26 +0200

    feat: Support all platforms
    Use psutil, also rank using Matcher

--- __init__.py
+++ __init__.py
@@ -2,12 +2,12 @@
 # Copyright (c) 2022 Manuel Schneider
 # Copyright (c) 2022 Benedict Dudel
 # Copyright (c) 2022 Pete Hamlin
-
-
+import dataclasses
 import os
 from signal import SIGKILL, SIGTERM
 
 from albert import *
+import psutil
 
 md_iid = "4.0"
 md_version = "2.0.1"
@@ -16,8 +16,13 @@ md_description = "Kill processes"
 md_license = "MIT"
 md_url = "https://github.com/albertlauncher/albert-plugin-python-kill"
 md_authors = ["@Pete-Hamlin", "@BenedictDudel", "@ManuelSchneid3r"]
-md_maintainers = ["@Pete-Hamlin"]
-md_platforms = ["Linux"]
+md_maintainers = ["@Pete-Hamlin", "@ManuelSchneid3r"]
+md_lib_dependencies = ["psutil"]
+
+@dataclasses.dataclass
+class RankedItem:
+    item: StandardItem
+    score: float
 
 
 class Plugin(PluginInstance, TriggerQueryHandler):
@@ -29,50 +34,47 @@ class Plugin(PluginInstance, TriggerQueryHandler):
         return "kill "
 
     def handleTriggerQuery(self, query):
-        if not query.isValid:
-            return
         results = []
         uid = os.getuid()
-        for dir_entry in os.scandir("/proc"):
+        matcher = Matcher(query.string)
+
+        for proc in psutil.process_iter(["pid", "name", "cmdline", "uids"]):
+
+            if not query.isValid:
+                return
+
             try:
-                if dir_entry.name.isdigit() and dir_entry.stat().st_uid == uid:
-                    proc_command = (
-                        open(os.path.join(dir_entry.path, "comm"), "r").read().strip()
-                    )
-                    if query.string in proc_command:
-                        debug(proc_command)
-                        proc_cmdline = (
-                            open(os.path.join(dir_entry.path, "cmdline"), "r")
-                            .read()
-                            .strip()
-                            .replace("\0", " ")
-                        )
-                        results.append(
-                            StandardItem(
-                                id="kill",
-                                icon_factory=lambda: makeGraphemeIcon("💀"),
-                                text=proc_command,
-                                subtext=proc_cmdline,
-                                actions=[
-                                    Action(
-                                        "terminate",
-                                        "Terminate process",
-                                        lambda pid=int(dir_entry.name): os.kill(
-                                            pid, SIGTERM
-                                        ),
-                                    ),
-                                    Action(
-                                        "kill",
-                                        "Kill process",
-                                        lambda pid=int(dir_entry.name): os.kill(
-                                            pid, SIGKILL
-                                        ),
-                                    ),
-                                ],
-                            )
-                        )
-            except FileNotFoundError:  # TOCTOU dirs may disappear
+                if proc.info["uids"].real != uid:
+                    continue
+
+                pid = proc.info["pid"]
+                name = proc.info["name"]
+                cmdline = " ".join(proc.info["cmdline"]) if proc.info["cmdline"] else ""
+
+                if match := matcher.match(name, cmdline):
+                    results.append(RankedItem(
+                        item=StandardItem(
+                            id="kill",
+                            icon_factory=lambda: makeGraphemeIcon("💀"),
+                            text=name,
+                            subtext=cmdline,
+                            actions=[
+                                Action(
+                                    "terminate",
+                                    "Terminate",
+                                    lambda pid_=pid: os.kill(pid_, SIGTERM),
+                                ),
+                                Action(
+                                    "kill",
+                                    "Kill",
+                                    lambda pid_=pid: os.kill(pid_, SIGKILL),
+                                )
+                            ]
+                        ),
+                        score=match.score
+                    ))
+            except (psutil.NoSuchProcess, psutil.AccessDenied, KeyError, AttributeError) as e:
+                warning(f"{e.__class__.__name__}: {e}")
                 continue
-            except IOError:  # TOCTOU dirs may disappear
-                continue
-        query.add(results)
+
+        query.add([r.item for r in sorted(results, key=lambda x: x.score, reverse=True)])
