e08a27950f61037b909c2b4617a04e00bf5be061
Manuel Schneider <manuelschneid3r@gmail.com>
2026-04-16 12:34:16 +0200

    feat: Fuzzy support
    And some refactoring for readability

--- __init__.py
+++ __init__.py
@@ -22,7 +22,7 @@ md_lib_dependencies = ["psutil"]
 @dataclasses.dataclass
 class RankedItem:
     item: StandardItem
-    score: float
+    match: Match
 
 
 class Plugin(PluginInstance, GeneratorQueryHandler):
@@ -30,52 +30,69 @@ class Plugin(PluginInstance, GeneratorQueryHandler):
     def __init__(self):
         PluginInstance.__init__(self)
         GeneratorQueryHandler.__init__(self)
+        self.fuzzy = False
+
+    def supportsFuzzyMatching(self):
+        return True
+
+    def setFuzzyMatching(self, enabled):
+        self.fuzzy = enabled
 
     def defaultTrigger(self):
         return "kill "
 
-    def items(self, ctx):
-        results = []
-        uid = os.getuid()
-        matcher = Matcher(ctx.query)
+    @dataclasses.dataclass
+    class Process:
+        pid: int
+        name: str
+        cmdline: str
 
+    @staticmethod
+    def _get_user_processes(uid):
         for proc in psutil.process_iter(["pid", "name", "cmdline", "uids"]):
-
-            if not ctx.isValid:
-                return
-
             try:
                 if proc.info["uids"].real != uid:
                     continue
 
-                pid = proc.info["pid"]
-                name = proc.info["name"]
-                cmdline = " ".join(proc.info["cmdline"]) if proc.info["cmdline"] else ""
-
-                if match := matcher.match(name, cmdline):
-                    results.append(RankedItem(
-                        item=StandardItem(
-                            id="kill",
-                            icon_factory=lambda: Icon.grapheme("💀"),
-                            text=name,
-                            subtext=cmdline,
-                            actions=[
-                                Action(
-                                    "terminate",
-                                    "Terminate",
-                                    lambda pid_=pid: os.kill(pid_, SIGTERM),
-                                ),
-                                Action(
-                                    "kill",
-                                    "Kill",
-                                    lambda pid_=pid: os.kill(pid_, SIGKILL),
-                                )
-                            ]
-                        ),
-                        score=match.score
-                    ))
+                yield Plugin.Process(
+                    pid=proc.info["pid"],
+                    name=proc.info["name"],
+                    cmdline=" ".join(proc.info["cmdline"]) if proc.info["cmdline"] else ""
+                )
             except (psutil.NoSuchProcess, psutil.AccessDenied, KeyError, AttributeError) as e:
                 warning(f"{e.__class__.__name__}: {e}")
                 continue
 
-        yield [r.item for r in sorted(results, key=lambda x: x.score, reverse=True)]
+    @staticmethod
+    def _make_item(proc: Process):
+        return StandardItem(
+            id="kill",
+            icon_factory=lambda: Icon.grapheme("💀"),
+            text=proc.name,
+            subtext=proc.cmdline,
+            actions=[
+                Action(
+                    "terminate",
+                    "Terminate",
+                    lambda pid_=proc.pid: os.kill(pid_, SIGTERM),
+                ),
+                Action(
+                    "kill",
+                    "Kill",
+                    lambda pid_=proc.pid: os.kill(pid_, SIGKILL),
+                )
+            ]
+        )
+
+    def items(self, ctx):
+        matcher = Matcher(ctx.query, MatchConfig(fuzzy=self.fuzzy))
+
+        rank_items = [
+            RankedItem(self._make_item(proc), matcher.match(proc.name, proc.cmdline))
+            for proc in self._get_user_processes(os.getuid())
+        ]
+
+        yield [
+            r.item for r in sorted(rank_items, key=lambda x: x.match.score, reverse=True)
+            if r.match.isMatch()
+        ]
